0166bccbf94a6c9dafdaab463e7ea9d29b6291a6
Jim Xu <jimxu@tiidian.com>
2018-08-01 10:18:13 +0800

    更新活动详情内容

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -20,24 +20,84 @@
     </div>
 
     <!-- 主题内容-->
-    <div class="content-wrapper"></div>
+    <div class="content-wrapper">
+      <div class="icon" :style="head_bg">
+        <!--<img :src="poiInfo.pic_url" alt="">-->
+      </div>
+      <div class="name">
+        <h3>{{poiInfo.name}}</h3>
+      </div>
+      <div class="collect">
+        <img src="../../../resource/img/star.png" alt="">
+        <span>收藏</span>
+      </div>
+    </div>
 
     <!-- 公告内容-->
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <img v-if="poiInfo.discounts2" :src="poiInfo.discounts2[0].icon_url" alt="" class="icon">
+      <span class="text" v-if="poiInfo.discounts2">{{poiInfo.discounts2[0].info}}</span>
+      <div class="detail" v-if="poiInfo.discounts2">
+        {{poiInfo.discounts2.length}}个活动
+        <span class="icon-keyboard_arrow_right"></span>
+      </div>
+    </div>
+
+    <div class="bg-wrapper" :style="content_bg">
+      <!--<img :src="poiInfo.head_pic_url" alt="">-->
+    </div>
+
+    <!-- 公告详情-->
+    <div class="bulletin-detail">
+      <div class="detail-wrapper">
+        <div class="content-wrapper">
+          <div class="icon" :style="head_bg"></div>
+          <h3 class="name">{{poiInfo.name}}</h3>
+          <!-- 评价 稍后-->
+          <p class="tip">
+            {{poiInfo.min_price_tip}}<i>|</i>
+            {{poiInfo.shipping_fee_tip}}<i>|</i>
+            {{poiInfo.delivery_time_tip}}
+          </p>
+          <p class="time">
+            配送时间：
+            {{poiInfo.shipping_time}}
+          </p>
+          <div class="discounts" v-if="poiInfo.discounts2">
+            <p>
+              <img :src="poiInfo.discounts2[0].icon_url" alt="">
+              <span>{{poiInfo.discounts2[0].info}}</span>
+            </p>
+          </div>
+        </div>
+
+        <div class="close-wrapper">
+          <span class="icon-close"></span>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
-    export default {
-        name: "Header",
-        props: {
-          poiInfo: {
-            type: Object,
-            default: {},
-          }
-        }
+  export default {
+    name: "Header",
+    props: {
+      poiInfo: {
+        type: Object,
+        default: {},
+      }
+    },
+    computed: {
+      content_bg() {
+        return "background-image: url(" + this.poiInfo.head_pic_url + ")"
+      },
+      head_bg() {
+        return "background-image: url(" + this.poiInfo.pic_url + ")"
+      }
     }
+  }
 </script>
 
 <style scoped>
